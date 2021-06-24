2fa600a0f0fa9a152cdf2762a9407ce5cca3a904
e.g. pavelka <pav@egpavelka.com>
2021-06-24 06:07:56 -0400

    bug fix - activity log management

--- api/resolvers/activity-log.ts
+++ api/resolvers/activity-log.ts
@@ -1,30 +1,39 @@
 import { IResolvers } from 'graphql-tools'
+import { authenticate } from '../actions/auth'
 import { findById, findAll } from '../actions/activity-log'
-
-
+import { findById as findLogOwner } from '../actions/entry'
+import {
+  ActivityLogInput,
+  CreateLogInput,
+  UpdateLogInput,
+} from 'shared/types/editor'
+import { Entry, User, ActivityLog } from 'shared/types'
+import Ctx from '../context'
 
 const ActivityLogResolvers: IResolvers = {
   Query: {
-      findActivityLogById: async (_, { id: number }, ctx) => {
+    findActivityLogById: async (_, { id: number }, ctx) => {
       return await findById(id)
     },
-      findAllActivityLogs: async (_, { entryId: number }, ctx) => {
+    findAllActivityLogs: async (_, { entryId: number }, ctx) => {
       return await findAll(entryId)
-    }
+    },
   },
-    Mutation: {
-    updateEntry: authenticate(
-      async (_, args: UpdateEntryInput, ctx: Ctx): Promise<Entry> => {
-        console.log('RESOLVERS -> ENTRY -> UPDATE ->')
-        return await update(ctx.user.id, ...args)
-      }
-    ),
+  Mutation: {
+    updateLog: async (
+      _,
+      args: UpdateLogInput,
+      ctx: Ctx
+    ): Promise<ActivityLog> => {
+      console.log('RESOLVERS -> ACTIVITY LOG -> UPDATE ->')
+      return await update(ctx.user.id, ...args)
     },
-    ActivityLog: {
-    user: async (entry, {}, ctx: Ctx): Promise<User> => {
-      return await findEntryOwner(entry.userId)
+  },
+  ActivityLog: {
+    entry: async (activityLog, {}, ctx: Ctx): Promise<Entry> => {
+      return await findLogOwner(activityLog.entryId)
     },
-    }
+  },
 }
 
 export default ActivityLogResolvers
