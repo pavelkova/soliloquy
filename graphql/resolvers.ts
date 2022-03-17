ce265c45033157535d8204dfeb221b1bc340a107
e.g. pavelka <pav@egpavelka.com>
2022-03-17 04:57:25 -0400

    optimize graphql resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -249,8 +249,52 @@ const resolvers: IResolvers = {
         }),
         updateOrCreateLog: authenticate(async (_, args, ctx): Promise<ActivityLog> => {
             try {
-                return ctx.prisma.activityLog.update({
-                })
+                async function getRecentLog () {
+                    if (args.logId) {
+                        return await ctx.prisma.activityLog.findUnique({
+                            wnere: {
+                                id: args.logId
+                            }
+                        })
+                    } else {
+                        return await ctx.prisma.activityLog.findFirst({
+                            where: {
+                                userId: ctx.user.id,
+                                date: args.date
+                            }
+                        })
+                    }
+                }
+                const now = new Date()
+                const recentLog = await getRecentLog()
+                function minutesAgo(prev: Date): number {
+                    const now = new Date()
+                    return Math.floor(((now - prev) / 1000) / 60)
+                }
+                if (recentLog && (minutesAgo(recentLog.updatedAt) < 5)) {
+                    return await ctx.prisma.activityLog.update({
+                        where: {
+                            id: recentLog.id
+                        },
+                        data: {
+                            updatedAt: now,
+                            content: args.content,
+                            wordCount: args.wordCount,
+                            lowestWordCount: args.lowestWordCount
+                        }
+                    })
+                } else {
+                    return await ctx.prisma.activityLog.create{
+                        data: {
+                            userId: ctx.user.id,
+                            createdAt: args.start,
+                            updatedAt: now,
+                            content: args.content,
+                            wordCount: args.wordCount,
+                            lowestWordCount: args.lowestWordCount
+                        }
+                    }
+                }
             } catch (e) {
             }
         }),
