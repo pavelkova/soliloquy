a2961ca0fe4a8b5b0b67729335dbf4cb12e7e322
e.g. pavelka <pav@egpavelka.com>
2022-05-12 04:30:12 -0400

    update user resolvers

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -1,20 +1,67 @@
 import { IResolvers } from 'graphql-tools'
 import { User, Settings } from 'shared/types'
-import { authenticate } from './helpers/auth'
+import { authenticate,  setUserToken, revokeUserToken } from './helpers/auth'
+import { encryptPassword, validatePassword } from './helpers/hashing'
+
+async function getUserByEmail(prisma, email:string): Promise<User|null> {
+    return await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+}
 
 const resolvers: IResolvers = {
     Query: {
+        currentUser: async (_, _, ctx): Promise<User> => {
+            try {
+                if (ctx.user) {
+                    return await ctx.prisma.user.findUnique({
+                        where: {
+                            id: ctx.user.id
+                        }
+                    })
+                } else {
+                    throw new Error("Not logged in")
+                }
+            } catch (e) {
+            }
+        },
     },
     Mutation: {
         signup: async (_, args, ctx): Promise<User> => {
             try {
-                return
+                const existingUser = await getUserByEmail(ctx.prisma, args.email)
+                if (existingUser) {
+                    throw new Error("A user with this email address already exists.")
+                } else {
+                    const hash = await encryptPassword(args.password)
+                    const user = await ctx.prisma.user.create({
+                        data: {
+                            email: args.email,
+                            name: args.name,
+                            password: hash,
+                            // settings:
+                        }
+                    })
+                    if (user) setUserToken(ctx.res, user)
+                    return user
+                }
             } catch (e) {
             }
         },
         login: async (_, args: { email: string, password: string }, ctx): Promise<User> => {
             try {
-                return
+                const user = await getUserByEmail(ctx.prisma, args.email)
+                if (user) {
+                    const validPassword = await validatePassword(user, args.password)
+                    if (validPassword) {
+                        return user
+                    }
+                } else {
+                    throw new Error("")
+                }
+
             } catch (e) {
             }
         },
